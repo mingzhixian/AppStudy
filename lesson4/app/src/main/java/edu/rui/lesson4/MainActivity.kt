e26ba2e0817fa870a403721c6dc1cdfe86330ab2
kic <mingzhixian123@gmail.com>
2022-04-07 16:43:10 +0800

    更新

--- lesson4/app/src/main/java/edu/rui/lesson4/MainActivity.kt
+++ lesson4/app/src/main/java/edu/rui/lesson4/MainActivity.kt
@@ -1,15 +1,19 @@
 package edu.rui.lesson4
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.marginBottom
+import androidx.core.view.marginTop
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.*
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -24,14 +28,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         //生成界面
         val recycler = findViewById<RecyclerView>(R.id.menu)
         recycler.layoutManager = LinearLayoutManager(this)
-        recycler.adapter = TodoAdapter(todoItems,this.findViewById<ConstraintLayout>(R.id.todo_detail))
+        recycler.adapter =
+            TodoAdapter(todoItems, this)
 
         //设置监听
         this.findViewById<FloatingActionButton>(R.id.todo_add).setOnClickListener(this)
+        this.findViewById<ConstraintLayout>(R.id.todo_detail).setOnClickListener(this)
         this.findViewById<TextView>(R.id.todo_detail_cancel).setOnClickListener(this)
         this.findViewById<TextView>(R.id.todo_detail_ok).setOnClickListener(this)
     }
 
+    //初始化数据
     private fun initData() {
         //数据库获取
         for (i in 0..4) {
@@ -42,23 +49,75 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    private fun deleteItem() {
-        //删除数据
-    }
+    //吸顶效果
+    private fun titleOnScroll() {
+        val title = this.findViewById<TextView>(R.id.todo_title)
+        val recyclerView = this.findViewById<RecyclerView>(R.id.menu)
+        recyclerView.addOnScrollListener(object : OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+            }
 
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (dy>0){
+                    if (title.marginTop>0){
+                        title.layoutParams.height=title.marginTop-dy
+                        return
+                    }else if (title.marginBottom>0){
+                        title.layoutParams.height=title.marginTop-dy
+                        return
+                    }else if (title.textSize>24){}
+                }
+            }
+        })
+    }
 
     //展示输入框
     override fun onClick(view: View?) {
         var id: Int? = view?.id
+        val todoDetail = this.findViewById<ConstraintLayout>(R.id.todo_detail)
+        val todoAdd = this.findViewById<FloatingActionButton>(R.id.todo_add)
         when (id) {
             R.id.todo_add -> {
-                this.findViewById<ConstraintLayout>(R.id.todo_detail).visibility = VISIBLE
+                todoAdd.visibility = GONE
+                todoDetail.apply {
+                    alpha = 0f
+                    visibility = VISIBLE
+                    animate()
+                        .alpha(1f)
+                        .setDuration(360.toLong())
+                        .setListener(null)
+                }
             }
             R.id.todo_detail_ok -> {
-                this.findViewById<ConstraintLayout>(R.id.todo_detail).visibility = GONE
+                todoDetail.apply {
+                    animate()
+                        .alpha(0f)
+                        .setDuration(360.toLong())
+                        .setListener(object : AnimatorListenerAdapter() {
+                            override fun onAnimationEnd(animation: Animator) {
+                                todoDetail.visibility = GONE
+                            }
+                        })
+                }
+                todoAdd.visibility = VISIBLE
             }
             R.id.todo_detail_cancel -> {
-                this.findViewById<ConstraintLayout>(R.id.todo_detail).visibility = GONE
+                todoDetail.apply {
+                    animate()
+                        .alpha(0f)
+                        .setDuration(360.toLong())
+                        .setListener(object : AnimatorListenerAdapter() {
+                            override fun onAnimationEnd(animation: Animator) {
+                                todoDetail.visibility = GONE
+                            }
+                        })
+                }
+                todoAdd.visibility = VISIBLE
+            }
+            R.id.todo_detail -> {
+                //拦截点击事件
             }
         }
     }
--- lesson4/app/src/main/java/edu/rui/lesson4/TodoAdapter.kt
+++ lesson4/app/src/main/java/edu/rui/lesson4/TodoAdapter.kt
@@ -1,5 +1,6 @@
 package edu.rui.lesson4
 
+import android.renderscript.Int2
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,16 +9,25 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class TodoAdapter(val todoItems: List<TodoItem>,detail: ConstraintLayout) : RecyclerView.Adapter<TodoAdapter.ViewHolder>() {
+class TodoAdapter(val todoItems: List<TodoItem>, main: MainActivity) :
+    RecyclerView.Adapter<TodoAdapter.ViewHolder>() {
+    val getdetail = main.findViewById<ConstraintLayout>(R.id.todo_detail)
+    val getadd=main.findViewById<FloatingActionButton>(R.id.todo_add)
+
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val content: TextView = view.findViewById(R.id.todo_item_content)
         val status: ImageView = view.findViewById(R.id.todo_item_status)
         val todoItem: LinearLayout = view.findViewById(R.id.todo_item)
-        val todoDetail: ConstraintLayout =detail
+        val todoDetail: ConstraintLayout = getdetail
+        val todoAdd:FloatingActionButton=getadd
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.todo_item, parent, false)
         return ViewHolder(view)
@@ -32,7 +42,15 @@ class TodoAdapter(val todoItems: List<TodoItem>,detail: ConstraintLayout) : Recy
             0 -> holder.status.setImageResource(R.drawable.status_todo)
         }
         holder.todoItem.setOnClickListener {
-            holder.todoDetail.visibility = View.VISIBLE
+            holder.todoDetail.apply {
+                alpha = 0f
+                visibility = View.VISIBLE
+                animate()
+                    .alpha(1f)
+                    .setDuration(360.toLong())
+                    .setListener(null)
+            }
+            holder.todoAdd.visibility=View.GONE
         }
     }
 
